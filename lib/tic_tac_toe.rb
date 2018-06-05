9870957f97e703e72095130a13172c4d1ba97061
Christopher Stull <chris.stull@gmail.com>
2018-06-05 18:19:29 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,4 +2,12 @@ class TicTacToe
   
   def initialize()
     @board = [" "]*9
-    
+  end
+  
+  def board
+    @board
+  end
+  
+  WIN_COMBINATIONS = [[0,1,2],[0,3,6],[0,4,8],[1,4,7],[2,4,6],[2,5,8]]
+  
+  
