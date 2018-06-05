fbb2b90e00f22233f18a8c188f3a3fa69b1f09ad
Christopher Stull <chris.stull@gmail.com>
2018-06-05 18:28:43 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -18,6 +18,8 @@ class TicTacToe
   
   WIN_COMBINATIONS = [[0,1,2],[0,3,6],[0,4,8],[1,4,7],[2,4,6],[2,5,8],[3,4,5],[6,7,8]]
   
-  
+  def input_to_index(user_input)
+    user_input.to_i - 1
+  end
   
 end
