3e1efa78064b8f479c12f87e70c974d38fd0f4ce
Christopher Stull <chris.stull@gmail.com>
2018-06-05 18:32:50 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -34,4 +34,21 @@ class TicTacToe
     index.between?(0,8) && !position_taken?(index)
   end
   
+  def turn_count(board)
+    turn_count = 0
+    board.each do |spot|
+      if spot == "X" || spot == "O"
+        turn_count += 1
+      end
+    end
+    return turn_count
+  end
+
+  def current_player(board)
+    if turn_count(board)%2 == 0
+      return "X"
+    else
+      return "O"
+    end
+  end
 end
